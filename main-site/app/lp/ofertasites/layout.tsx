9043305c9256adf4e9a6ceff78c8d87bccdca5ef
inp-jpedroforti <j.forti@inperson.com.br>
2026-03-23 17:32:48 -0300

    temp

---
+++ main-site/app/lp/ofertasites/layout.tsx
@@ -0,0 +1,10 @@
+import type { ReactNode } from "react";
+import "../../sites/sites-landing.css";
+
+export default function OfertaSitesLayout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  return children;
+}
