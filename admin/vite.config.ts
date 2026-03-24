0a13078faac8ec1065a8209cede7a47274489773
inp-jpedroforti <j.forti@inperson.com.br>
2026-03-24 15:05:31 -0300

    bg url

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "http://bgadmin.forticorp.com.br",
         changeOrigin: true,
       },
     },
